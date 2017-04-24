8baad2636910f774494e2a8c1ada91c1e92312f8
Крылов Данил <d.krylov@open-bs.ru>
2017-04-24 18:54:27 +0300

    Button

--- app/src/main/java/ru/test/danil/newsviwer/view/RecyclerViewAdapter.java
+++ app/src/main/java/ru/test/danil/newsviwer/view/RecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -45,7 +46,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         final News news = newsList.get(i);
         viewHolder.title.setText(news.getTitle());
         viewHolder.text.setText(news.getText());
-        viewHolder.text.setOnClickListener(new View.OnClickListener() {
+        viewHolder.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 presenter.clickNews(news);
@@ -62,11 +63,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         private TextView title;
         private TextView text;
+        private Button button;
 
         public ViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.textViewTitle);
             text = (TextView) itemView.findViewById(R.id.textViewText);
+            button = (Button) itemView.findViewById(R.id.buttonDetail);
         }
     }
 
